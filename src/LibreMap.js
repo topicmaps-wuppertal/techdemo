e808d58bbf38f3bc0516830de67663135dacd7f9
helllth <th@cismet.de>
2022-09-20 11:58:01 +0200

    add terrain experiment

--- src/LibreMap.js
+++ src/LibreMap.js
@@ -2,13 +2,14 @@ import React, { useRef, useEffect, useState } from "react";
 import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import "./mapLibre.css";
+import { Button } from "react-bootstrap";
 
 export default function Map(props = { opacity: 0.1 }) {
   const mapContainer = useRef(null);
   const map = useRef(null);
   const [lng] = useState(7.150764);
   const [lat] = useState(51.256);
-  const [zoom] = useState(14);
+  const [zoom] = useState(15);
   let opacity, textOpacity, iconOpacity; // } = props;
 
   //   opacity = 0.5;
@@ -21,6 +22,8 @@ export default function Map(props = { opacity: 0.1 }) {
       style: `https://omt.map-hosting.de/styles/osm-bright/style.json`,
       center: [lng, lat],
       zoom: zoom,
+      opacity: 0.5,
+      maxZoom: 19,
     });
     console.log("map.current", map.current);
 
@@ -47,49 +50,84 @@ export default function Map(props = { opacity: 0.1 }) {
         ],
         tileSize: 256,
       });
-      map.current.addLayer(
-        {
-          id: "wms-test-layer",
-          type: "raster",
-          opacity: 0.5,
-          source: "wms-test-source",
-          paint: {},
-        },
-        "waterway-name"
-      );
-      map.current.addLayer(
-        {
-          id: "3d-buildings",
-          source: "openmaptiles",
-          "source-layer": "building",
+      map.current.addSource("terrainSource", {
+        type: "raster-dem",
+        tiles: ["https://wuppertal-terrain.cismet.de/services/wupp_dgm_01/tiles/{z}/{x}/{y}.png"],
+        tileSize: 512,
+        maxzoom: 15,
+      });
+      map.current.addSource("hillshadeSource", {
+        type: "raster-dem",
+        tiles: ["https://wuppertal-terrain.cismet.de/services/wupp_dgm_01/tiles/{z}/{x}/{y}.png"],
+        tileSize: 512,
+        maxzoom: 15,
+      });
 
-          type: "fill-extrusion",
-          minzoom: 15,
-          paint: {
-            "fill-extrusion-color": "#aaa",
-            "fill-extrusion-height": {
-              type: "identity",
-              property: "render_height",
-            },
-            "fill-extrusion-base": {
-              type: "identity",
-              property: "render_min_height",
-            },
-            "fill-extrusion-opacity": 0.6,
+      // Layers ------------------------------------------------------------------------------
+      map.current.addLayer({
+        id: "wms-test-layer",
+        type: "raster",
+        opacity: 0.25,
+
+        source: "wms-test-source",
+        paint: { "raster-opacity": 0.5 },
+      });
+
+      map.current.addLayer({
+        id: "hillshade",
+        type: "hillshade",
+        source: "hillshadeSource",
+        layout: { visibility: "visible" },
+        paint: {
+          "hillshade-accent-color": "#5a5a5a",
+          "hillshade-exaggeration": 0.5,
+          "hillshade-highlight-color": "#FFFFFF",
+          "hillshade-illumination-anchor": "viewport",
+          "hillshade-illumination-direction": 335,
+          "hillshade-shadow-color": "#5a5a5a",
+        },
+      });
+
+      map.current.addLayer({
+        id: "3d-buildings",
+        source: "openmaptiles",
+        "source-layer": "building",
+
+        type: "fill-extrusion",
+        minzoom: 15,
+        paint: {
+          "fill-extrusion-color": "#aaa",
+          "fill-extrusion-height": {
+            type: "identity",
+            property: "render_height",
           },
+          "fill-extrusion-base": {
+            type: "identity",
+            property: "render_min_height",
+          },
+          "fill-extrusion-opacity": 0.5,
         },
-        "waterway-name"
-      );
+      });
 
-      map.current.setPaintProperty("wms-test-layer", "raster-opacity", 0.5);
+      console.log("map.current", map.current);
     });
 
     map.current.addControl(new maplibregl.NavigationControl(), "top-left");
+    map.current.addControl(
+      new maplibregl.TerrainControl({
+        source: "terrainSource",
+        maxzoom: 16,
+
+        exaggeration: 1,
+      }),
+      "top-left"
+    );
   });
 
   return (
     <div className='map-wrap'>
       <div ref={mapContainer} className='map' />
+      <Button>askdjh</Button>
     </div>
   );
 }
